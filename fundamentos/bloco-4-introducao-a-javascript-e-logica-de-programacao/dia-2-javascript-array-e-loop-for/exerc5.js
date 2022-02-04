b8906272cae16de07190f334c43b4b124b90ae0a
Renê Alves <renepvh11@gmail.com>
2022-02-04 11:04:53 -0400

    altera no exerc5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exerc5.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exerc5.js
@@ -1,9 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 
-var maior = numbers[0];
+let maior = numbers[0];
 
-for (var i = 0; i < numbers.length; i++) {
+for (let i = 0; i < numbers.length; i+= 1) {
    if ( numbers[i] > maior ) {
       maior = numbers[i];
    }
