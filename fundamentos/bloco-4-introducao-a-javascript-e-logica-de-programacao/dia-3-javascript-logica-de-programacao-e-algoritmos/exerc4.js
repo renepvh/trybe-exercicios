f9499826d6d85d7d4d5114b2db2adf15442526c2
Renê Alves <renepvh11@gmail.com>
2022-02-04 11:30:50 -0400

    adic exer 4

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exerc4.js
@@ -0,0 +1,14 @@
+let limite = 15;
+
+for (let numero =2; numero <= limite; numero++){
+  let ehPrimo = true;
+  for (let divisor = 2; divisor < numero; divisor++) {
+    if (numero % divisor === 0) {
+      ehPrimo = false;
+      break;
+    }
+  }
+  if (ehPrimo) {
+    console.log(numero);
+  }
+}
