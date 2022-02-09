2c508e32f48a69dbe9f6bc786a6a7140abb2d727
Renê Alves <renepvh11@gmail.com>
2022-02-09 17:25:49 -0400

    primeiro commit e adiciona exercaula

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -1,4 +1,29 @@
 
 document.getElementById("header-container").style.background = "#00b069";
 
-document.getElementsByTagName("h3").style.background = "blue"
+let emerg = document.querySelectorAll(".emergency-tasks");
+
+for (let i = 0; i < emerg.length; i +=1) {
+  emerg[i].style.background = "#ff9f84";
+}
+
+let roxo = document.querySelectorAll(".h3roxo");
+
+for (i = 0; i < roxo.length; i += 1) {
+  roxo[i].style.background = "#a500f3";
+}
+
+
+let semEmerg = document.getElementsByClassName("no-emergency-tasks");
+
+for (i = 0; i < semEmerg.length; i += 1) {
+  semEmerg[i].style.background = "#f9db5e";
+}
+
+let black = document.querySelectorAll(".h3black");
+
+for (i = 0; i < black.length; i += 1) {
+  black[i].style.background = "black"
+}
+
+document.getElementById("footer-container").style.background = "#003533"
