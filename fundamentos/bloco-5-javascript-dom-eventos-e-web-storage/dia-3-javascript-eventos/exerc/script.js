e5761833610ffed1317666e61812fc677af837f8
Renê Alves <renepvh11@gmail.com>
2022-02-11 18:08:50 -0400

    tentando resolver a questao 1

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exerc/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exerc/script.js
@@ -14,9 +14,31 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+//Exercicio 1
+window.onload = criaDias
 
-const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
-for (let index = 0; index < dezDaysList.length; index += 1) {
-  
-}
+function criaDias(){
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+
+
+  let days = document.getElementById("days");
+
+  for (let index = 0; index < dezDaysList.length; index += 1){
+    let dayItem = dezDaysList[index];
+
+    let diaLista = document.createElement("li");
+
+    diaLista.innerText = dayItem;
+    diaLista.className = "day"
+
+    days.appendChild(diaLista)
+
+  //   if (diaLista[index].innerText === 24) {
+  //     diaLista.className = "holiday"
+  //   };
+  }
+
+
+};
