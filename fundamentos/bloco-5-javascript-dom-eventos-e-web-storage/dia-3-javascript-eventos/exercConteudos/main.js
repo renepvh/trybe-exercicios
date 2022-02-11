a11376ced7068366e956a4d25f77788b3ed6c665
Renê Alves <renepvh11@gmail.com>
2022-02-11 14:47:30 -0400

    resolvendo os exercicios

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercConteudos/main.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercConteudos/main.js
@@ -29,12 +29,27 @@ thirdLi.addEventListener("click", receberClick);
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
 
+input.addEventListener("input", function(eventoDeOrigem){
+  let estiloCss = document.querySelector(".tech");
+  estiloCss.innerText = eventoDeOrigem.target.value;
+});
+
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
 
+myWebpage.addEventListener("dblclick", function(){
+  window.location.replace("https://renepvh.github.io/");
+});
+
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+
+myWebpage.addEventListener("mouseover", function mouseAlteraCor () {
+  myWebpage.style.color = "red"
+})
+
+
 
 // Segue abaixo um exemplo do uso de event.target:
 
